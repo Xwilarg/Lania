f601c59b9202a9007f93a4b6488b73bc203a641a
Xwilarg <xwilarg@yahoo.fr>
2018-06-10 21:09:38 +0200

    Update help with stats command

--- Lania/Sentences.cs
+++ Lania/Sentences.cs
@@ -9,7 +9,8 @@ namespace Lania
         public readonly static string hiStr = "Hi.";
         public readonly static string help = "You can use the commands 'Open gate' and 'Close gate' to manage it." + Environment.NewLine +
                                             "You will receive images from the game, you can react to them using reactions." + Environment.NewLine +
-                                            "You can also send images by posting them on the channel where the gate is open, and see the reactions that people add on them.";
+                                            "You can also send images by posting them on the channel where the gate is open, and see the reactions that people add on them." + Environment.NewLine +
+                                            "Then you can use the command 'Stats gate' to see what reactions you received the most";
         public static string onlyUser(string user) { return ("Only " + user + " can do this command.");  }
         public readonly static string noEmote = "You didn't receive any emote yet.";
         public readonly static string myEmotes = "Here are the 10 emotes your received the most:";
