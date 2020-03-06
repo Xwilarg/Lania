ade72ecaedbb7939cd77de11f0795a3aba28dfa1
Xwilarg <xwilarg@yahoo.fr>
2020-03-06 22:23:56 +0100

    Add working bot

---
+++ LaniaV2/Modules/CommunicationModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using DiscordUtils;
+using System.Threading.Tasks;
+
+namespace LaniaV2.Modules
+{
+    public class CommunicationModule : ModuleBase
+    {
+        [Command("Info")]
+        public async Task Info()
+        {
+            await ReplyAsync(embed: Utils.GetBotInfo(Program.P.StartTime, "Lania", Program.P.client.CurrentUser));
+        }
+    }
+}
--- LaniaV2/Program.cs
+++ LaniaV2/Program.cs
@@ -1,9 +1,62 @@
-﻿namespace LaniaV2
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordUtils;
+using LaniaV2.Modules;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LaniaV2
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
+               => await new Program().MainAsync();
+
+        public readonly DiscordSocketClient client;
+        private readonly CommandService commands = new CommandService();
+
+        public DateTime StartTime { private set; get; }
+        public static Program P { private set; get; }
+
+        private Program()
         {
+            P = this;
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+            });
+            client.Log += Utils.Log;
+            commands.Log += Utils.LogError;
+        }
+
+        private async Task MainAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+
+            await commands.AddModuleAsync<CommunicationModule>(null);
+            dynamic json = JsonConvert.DeserializeObject(File.ReadAllText("Keys/Credentials.json"));
+            if (json.botToken == null)
+                throw new NullReferenceException("Your Credentials.json is missing mandatory information, it must at least contains botToken");
+            await client.LoginAsync(TokenType.Bot, (string)json.botToken);
+            StartTime = DateTime.Now;
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            if (msg == null || arg.Author.IsBot) return;
+            int pos = 0;
+            if (msg.HasMentionPrefix(client.CurrentUser, ref pos) || msg.HasStringPrefix("l.", ref pos))
+            {
+                SocketCommandContext context = new SocketCommandContext(client, msg);
+                await commands.ExecuteAsync(context, pos, null);
+            }
         }
     }
 }
